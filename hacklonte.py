7fcc0ead1356ba93be00d71f44af05c68fd0a306
vRimuru-sama <62886531+vRimuru-sama@users.noreply.github.com>
2020-03-30 20:58:25 +0700

    Lonte
    Hack lonte

---
+++ hacklonte.py
@@ -0,0 +1,55 @@
+print("hello selamat datang")
+
+print ("_____ _   _  ____ _____ _  __  _ _ _")
+print ("|  ___| | | |/ ___| ____| |/ / | | | |")
+print ("| |_  | | | | |   |  _| | ' /  | | | |")
+print ("|  _| | |_| | |___| |___| . \  |_|_|_|")
+print("|_|    \___/ \____|_____|_|\_\ (_|_|_)")
+{
+  print ("====================================")
+}
+{
+  print ("author = Rimuru-sama")
+}
+{
+  print ("masih smp loh goblox!!")
+}
+{
+ print ("team = loli team")
+}
+{
+  print ("author nya baik dan jelek")
+}
+{
+ print ("coppy gw bunuh luh tod") 
+}
+{
+  print ("love you 😘")
+}
+#hello qimak
+#nama gua Rimuru
+#semogi sehat yah
+{
+  print ("====================================")
+}
+print ("pilih")
+print (" no 1 hack bokep")
+print ("no 2 pesan lonte")
+print ("no tiga pesan loli ")
+print (" pergi lo no 4")
+{
+  print ("=====================================")
+}
+{
+  input ('masukan nomor =')
+}
+# output nomor 1
+{
+ print("maaf nyet lu di pranak kalo nau?")
+}
+{
+  print("nyewa dulu lah goblox")
+}
+{
+  print("[+]kwkwk Rimuru-sama kwkwkwk[+]")
+}
